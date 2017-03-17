43f88c631c360821223371b1e6cb5895cfdd9dd8
Leticia Choo <leticia.choo@temasys.com.sg>
2017-03-18 03:43:21 +0800

    ESS-590 Updated docs.

--- doc-theme/scripts/main.js
+++ doc-theme/scripts/main.js
@@ -1,10 +1,29 @@
 $(document).ready(function () {
 
   $('#side-menu').on('click', 'a', function () {
-    if (!$(this).attr('target-menu')) return;
+    if (!$(this).attr('target-menu')) {
+      if ($(this).attr('template')) {
+        $('[template].active').removeClass('active');
+        $(this).toggleClass('active');
+        $('#load-template').load('templates/' + $(this).attr('template'));
+      }
+      return;
+    }
     $(this).toggleClass('active');
     $(this).find('.fa').toggleClass('fa-caret-down').toggleClass('fa-caret-up');
     $('#' + $(this).attr('target-menu')).slideToggle();
   });
 
+  $('#load-template').load('templates/overview.html');
+
+  // HOTFIX: :(
+  function onResize () {
+    $('.container navbar.side, .container .body').height($(window).height() - $('navbar.top').outerHeight());
+    //$('.container, .container-wrapper').outerHeight($(window).height() - $('navbar.top').outerHeight());
+  }
+
+  $(window).resize(onResize);
+  onResize();
+  
+  $(document).prettyPrint();
 });
